0ca83e17c7369df219df70bff64d3ed5f7916ccc
Paul <paveldanilsky@yandex.ru>
2026-04-21 10:27:41 +0500

    fix: удаление чувствительных данных из merge

---
+++ app/core/config.py
@@ -0,0 +1,22 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class Settings(BaseSettings):
+    POSTGRES_USER: str
+    POSTGRES_PASSWORD: str
+    POSTGRES_HOST: str
+    POSTGRES_PORT: int
+    POSTGRES_DB: str
+
+    @property
+    def database_url_async(self) -> str:
+        return f"postgresql+asyncpg://{self.POSTGRES_USER}:{self.POSTGRES_PASSWORD}@{self.POSTGRES_HOST}:{self.POSTGRES_PORT}/{self.POSTGRES_DB}"
+
+    @property
+    def database_url_sync(self) -> str:
+        return f"postgresql://{self.POSTGRES_USER}:{self.POSTGRES_PASSWORD}@{self.POSTGRES_HOST}:{self.POSTGRES_PORT}/{self.POSTGRES_DB}"
+
+    model_config = SettingsConfigDict(env_file=".env", extra="ignore")
+
+
+settings = Settings()
--- app/core/db.py
+++ app/core/db.py
@@ -1,30 +1,15 @@
-import os
-
-from dotenv import load_dotenv
 from sqlalchemy.ext.asyncio import create_async_engine
 
+from app.core.config import settings
 from app.core.logger_config import logger
 
-load_dotenv()
-
-user = os.getenv("POSTGRES_USER")
-password = os.getenv("POSTGRES_PASSWORD")
-host = os.getenv("POSTGRES_HOST")
-port = os.getenv("POSTGRES_PORT")
-db = os.getenv("POSTGRES_DB")
-
-DATABASE_URL = os.getenv(
-    "DATABASE_URL", f"postgresql+asyncpg://{user}:{password}@{host}:{port}/{db}"
-)
-
-SYNC_DATABASE_URL = f"postgresql://{user}:{password}@{host}:{port}/{db}"
-
-engine = create_async_engine(DATABASE_URL, connect_args={"ssl": False})
+engine = create_async_engine(settings.database_url_async)
 
 
 async def check_connection():
     try:
         async with engine.connect():
             logger.info("Соединение установлено")
-    except Exception as e:
-        logger.info(f"Ошибка подключения: {e}")
+    except Exception:
+        logger.exception("Ошибка подключения")
+        raise
